782ddc2eeb5d50a7c4b2823b140ab54ba80f5b3d
George G <gmgale@icloud.com>
2020-09-08 10:51:55 +0200

    Added abilty to process files larger than 512 bits

--- SHA-1.py
+++ SHA-1.py
@@ -1,53 +1,94 @@
 import math, binascii, random
 
-# Read in .txt file and convert to hex
-in_file = open("fileToBeHashed.txt", "r")
 
-binary_file = int(''.join(format(ord(x), 'b') for x in in_file.read()))
-hex_file = hex(binary_file)
+
+# Some bitwise logical functions.
+def ROTL(x, n, w):
+  return((x << n & (2 ** w - 1)) | (x >> w - n))
+
+def Ch(x, y, z):
+  return((x & y) ^ (~x & z))
+
+def Parity(x, y, z):
+  return(x ^ y ^ z)
+
+def Maj(x, y, z):
+  return((x & y) ^ (x & z) ^ (y & z))
+
+
+
+
+
+
+
+# Spec defines consts in hex to fill list k of length 80 split as below.
+K = []
+
+for t in range(80):
+    if  t <= 19:
+        K.append(0x5a827999)
+    elif    t <= 39:
+        K.append(0x6ed9eba1)
+    elif    t <= 59:
+        K.append(0x8f1bbcdc)
+    else:
+        K.append(0xca62c1d6)
+
+# Spec defines values for H.
+H = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0]
+
+
+
+
+
+# Read in .txt file and convert to hex.
+in_file = open("small.txt", "r")
+binary_file = str(''.join(format(ord(x), 'b') for x in in_file.read()))
 #print(binary_file)
 #print(hex_file)
 
-# Generate 5x 8-char hex values for later
-H = []
-for i in range(5):
-    x = hex(random.randint(0, 4294967295))
-    H.append(x[2:])
-#print(H)
-
-# Pad binary file until 448bits long
-bin_file_str = str(binary_file)
-bin_file_str += "1"
-
-while len(bin_file_str) < 448:
-    bin_file_str += "0"
-#print(bin_file_str)
-
-# Add the length of the message in 64 bits to the end, totaling 512 bits.
-# '{:064b}'.format(binary_file)
-bin_file_length = str(binary_file)
-while len(bin_file_length) < 64:
-    bin_file_length = "0" + bin_file_length
-
-# M = 512 bits. Message + 1 + Padding 0's + 64 bits of message length
-# padded with 0's on left
-M = bin_file_str + bin_file_length
-#print(M)
 
 
-# The input above is then split into 512 bit chunks (only 1 512 bit in this instance) - 
-# Skipping that...
+
+
+# Add the length of the message in binary up to 64 bits to the end, totaling 512 bits.
+bin_file_length = bin(len(binary_file))
+bin_pad = str(bin_file_length[2:]) # Remove the "0b" from binary conversion.
+
+
+while len(bin_pad) <= 63:
+    bin_pad = "0" + bin_pad
+
+
+
+# Pad binary file until its a multiple of 512 bits including padding. "1" will be added below.
+while (len(binary_file) + 1 + len(bin_pad)) % 512 != 0:
+    bin_pad = "0" + bin_pad
+
+# Padding starts with a "1".
+bin_pad = "1" + bin_pad
+# Add the padding to the file
+binary_file = binary_file + bin_pad
+
+# The input above is then split into 512 bit chunks.
+M = []
+i = 0
+
+
+while i + 512 <= len(binary_file):
+    M.append(binary_file[i : i + 512])
+    i += 512
+
+print(M)
 # Each chunk is then split into 16x 32 bit words.
 W = []
 start = 0
 end = 31
 
-while end <= 512:
-    
-    W.append(M[start:end])
-    start += 32
-    end += 32
-
 
 # Let the hash begin.......
 
+
+
+
+#print(x)
